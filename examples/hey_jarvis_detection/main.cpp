5408c68fa37b8211017cc701733958939cd4c3c9
0xD34D <clark@scheffsblend.com>
2024-12-20 22:58:24 -0800

    use a more reasonable tensor arena size

--- examples/hey_jarvis_detection/main.cpp
+++ examples/hey_jarvis_detection/main.cpp
@@ -35,8 +35,7 @@ void wakeWordDetectionTask(void *params) {
   microphone.set_sample_rate(SAMPLE_RATE_HZ);
   microphone.set_bits_per_sample(I2S_DATA_BIT_WIDTH_32BIT);
   wakeWord.set_microphone(&microphone);
-  wakeWord.add_wake_word_model(model, 0.97f, 5, "Hey Jarvis",
-                               hey_jarvis_tflite_len);
+  wakeWord.add_wake_word_model(model, 0.97f, 5, "Hey Jarvis", 22940);
   wakeWord.set_features_step_size(10);
   wakeWord.add_detection_callback(&wakeWordDetected);
 
