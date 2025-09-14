4604be7e89852ddd248190f6217a8c1d7656255a
Nguyen Tuan Kiet <ngtuankiet.2212@gmail.com>
2025-09-15 00:47:20 +0700

    Update i2s_audio_speaker.h

--- include/esphome/components/i2s_audio/speaker/i2s_audio_speaker.h
+++ include/esphome/components/i2s_audio/speaker/i2s_audio_speaker.h
@@ -3,17 +3,16 @@
 #ifdef USE_ESP32
 
 #include "../i2s_audio.h"
+#include "driver/gpio.h"  // Added for GPIO_NUM_NC
 
 #include "esphome/components/speaker/speaker.h"
 #include "esphome/core/component.h"
-#include <driver/gpio.h>
 
 namespace esphome
 {
     namespace i2s_audio
     {
 
-        // Forward declare the Component class's methods to avoid ambiguity
         class I2SAudioSpeaker : public I2SAudioComponent, public speaker::Speaker
         {
         public:
@@ -24,20 +23,19 @@ namespace esphome
             void loop() override;
 
             void set_dout_pin(gpio_num_t pin) { this->dout_pin_ = pin; }
+            void set_sd_pin(gpio_num_t pin) { this->sd_pin_ = pin; } 
 
-            size_t write(const uint8_t *data, size_t length) override; // Changed from bool to size_t
+            size_t write(const uint8_t *data, size_t length) override;
 
             void set_channel(i2s_chan_config_t config) { this->channel_config_ = config; }
             void set_sample_rate(uint32_t sample_rate) { this->sample_rate_ = sample_rate; }
             void set_bits_per_sample(i2s_data_bit_width_t bits_per_sample) { this->bits_per_sample_ = bits_per_sample; }
             void set_external_dac(bool external_dac) { this->external_dac_ = external_dac; }
-            void set_sd_pin(gpio_num_t pin)
-            {
-                this->sd_pin_ = pin;
-                this->sd_pin_set_ = true;
+            void set_volume(float volume) {
+                if (volume < 0.0f) volume = 0.0f;
+                if (volume > 1.0f) volume = 1.0f;
+                this->volume_ = volume;
             }
-            void set_volume(float volume);
-            // Explicitly resolve ambiguous method calls by forwarding to the Speaker implementation
             bool is_failed() const { return speaker::Speaker::is_failed(); }
 
         protected:
@@ -46,14 +44,13 @@ namespace esphome
             void write_();
 
             gpio_num_t dout_pin_{I2S_GPIO_UNUSED};
-            bool external_dac_{true}; // Default to external DAC mode
+            gpio_num_t sd_pin_{GPIO_NUM_NC};  // NEW: SD pin (default to not connected)
+            bool external_dac_{true};
             i2s_chan_config_t channel_config_;
             i2s_chan_handle_t channel_;
             uint32_t sample_rate_;
             i2s_data_bit_width_t bits_per_sample_;
-            gpio_num_t sd_pin_{GPIO_NUM_NC}; // default unused
-            bool sd_pin_set_{false};
-            float volume_{1.0f}; // Default to 100% volume
+            float volume_{1.0f};  // Default to full volume
             HighFrequencyLoopRequester high_freq_;
         };
 
